879fc938b2217961d2744ee126614963db278abf
rag2621 <149160783+rag2621@users.noreply.github.com>
2025-09-21 02:02:07 +0530

    Update sensorModal.js

--- backend/modals/sensorModal.js
+++ backend/modals/sensorModal.js
@@ -45,13 +45,20 @@ const AuthoritySchema = new mongoose.Schema({
 });
 
 const BreakdownSchema = new mongoose.Schema({
-    node_id: { type: String, required: true },
-    breakdown_message: { type: String, required: true },
-    current: { type: Number, required: true },
-    voltage_present: { type: Boolean, required: true },
-    relay_status: { type: String, enum: ['ON', 'OFF', 'FAULT'], required: true },
-    gps: { type: GpsSchema, required: true },
-    timestamp: { type: Date, default: Date.now },
+    outNodeID: {
+    type: String,
+    required: true,
+    maxlength: 9
+  },
+  inNodeID: {
+    type: String,
+    required: true,
+    maxlength: 9
+  },
+  issueID: {
+    type: Number,
+    required: true
+  }
 }, {
     timestamps: true
 });
@@ -72,9 +79,29 @@ const nodeLocationSchema = new mongoose.Schema({
 });
 
 
+const dataHistorySchema = new mongoose.Schema({
+  nodeId: {
+    type: String,
+    required: true,
+    maxlength: 9,
+    index: true
+  },
+  current: [{
+    type: Number,
+    required: true
+  }]
+}, {
+  timestamps: true
+});
+
+
+
+
+
 module.exports = {
     SensorData: mongoose.model('SensorData', SensorDataSchema),
     Authority: mongoose.model('Authority', AuthoritySchema),
     Breakdown_data: mongoose.model('Breakdown_data', BreakdownSchema),
     NodeLocation : mongoose.model('NodeLocation', nodeLocationSchema),
+     DataHistory : mongoose.model('DataHistory', dataHistorySchema)
 };
